ce48e379da86dec70aaaa1bb89f8a7e125ca22b7
josepg2 <georgejoseph2sep92@gmail.com>
2016-01-30 20:12:09 +0530

    Added views

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+//Configure App
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Use Middleware
+
+//define Routes
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.listen(1337, function(){
+  console.log("Server started at port 1337");
+});
+
+
+console.log("Test");
